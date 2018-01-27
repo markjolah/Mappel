16b93f3f1d7741070ec080e841d4feaa44499bb9
Mark J. Olah <markjolah@users.noreply.github.com>
2018-01-26 19:35:11 -0700

    Python build system updates.

---
+++ src/python/Gauss1DMLE_module.cpp
@@ -0,0 +1,15 @@
+/** @file Gauss1DMLE_module.cpp
+ * @author Mark J. Olah (mjo\@cs.unm.edu)
+ * @date 2018
+ * @brief The instantiation of the mappel python module for Gauss1DMLE
+ */
+#include "Python.h"
+#include "py11_mappel_iface.h"
+#include "Gauss1DMLE.h"
+
+PYBIND11_MODULE(_Gauss1DMLE, M)
+{
+    M.doc()="1D Gaussian PSF model with fixed sigma and Poisson image noise";
+    mappel::python::bindMappelModel<mappel::Gauss1DMLE>(M);
+}
+
