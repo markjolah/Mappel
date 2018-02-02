1b70b06998e8a73941cef85e862f3cffb302913d
prelich <physx.grad@gmail.com>
2018-02-02 12:21:43 -0500

    adding more definitions

--- python/mappel/MappelBase.py
+++ python/mappel/MappelBase.py
@@ -21,8 +21,10 @@ class MappelBase(Gauss1DMLE):
         return self.get_stats
     
     # Return a dictionary of keys hyperparam_desc and values hyperparams
-#     def getHyperParameters(self):
-#        return
+     def getHyperParameters(self):
+         keys = self.hyperparams_desc
+         values = self.hyperparams
+         return dict(zip(keys, values))
     
 #     def setHyperParameters(hyperparams):
 #        return
@@ -39,29 +41,21 @@ class MappelBase(Gauss1DMLE):
     def modelImage(theta):
         return self.modelImage(theta)
     
-#     def modelDipImage(theta):
-#          return
-    
     def simulateImage(self,count=1,theta=None):
         if theta is None:
             theta = self.sample_prior(count)
         return self.simulate_image(theta)
     
-#     def simulatueDipImage():
-#          return
-    
-    def LLH(image, theta=None):
-        if theta is None:
-            print('function requires two arguments, image and parameter values')
-            pass
+    def LLH(image, theta):
+        self.__checkinputs({"image":image,"theta":theta})
         return self.objective_llh(image,theta)
     
     def modelGrad(image, theta):
-        # make general function for input checks.
+        self.__checkinputs({"image":image, "theta":theta})
         return self.objective_grad(image,theta)
     
     def modelHessian(image, theta):
-        # make general function for input checks
+        self.__checkinputs({"image":image, "theta":theta})
         return self.objective_hessian(image,theta)
     
 #     def modelObjective(image, theta, negate):
@@ -80,19 +74,19 @@ class MappelBase(Gauss1DMLE):
 #        return
     
     def observedInformation(image, theta):
-        # make general function for input checks
+        self.__checkinputs({"image":image, "theta":theta})
         return self.observed_information(image, theta)
     
 #     def scoreFunction(im, theta):
 #        return
     
-    def estimate(self, image, estimator_name='Newton', theta_init=None):
-        if theta_init is None:
-            theta_init = np.ones([3,image.shape[1]])
+    def estimate(self, image, theta_init, estimator_name='Newton'):
+        self.__checkinputs({"image":image, "theta_init":theta_init})
         return self.estimate_max(image, estimator_name, theta_init)
     
-#     def estimateDebug(image, estimator_name, theta_init):
-#        return
+    def estimateDebug(image, theta_init, estimator_name="Newton"):
+        self.__checkinputs({"image":image, "theta_init":theta_init})
+        return self.estimate_max_debug(image, estimator
     
 #     def estimatePosterior(image, max_samples, theta_init):
 #        return
@@ -100,67 +94,24 @@ class MappelBase(Gauss1DMLE):
 #     def estimagePosteriorDebug(image, max_samples, theta_init):
 #        return
     
-#     def uniformBackgroundModelLLH(ims):
-#        return
-    
-#     def modelComparisonUniform(alpha, ims, theta_mle):
-#        return
-    
-#     def noiseBackgroundModelLLH(ims):
-#        return
-    
-#     def modelComparisonNoise(alpha, ims, theta_mle):
-#        return
-    
-#     def evaluateEstimatorAt(estimator, theta, nTrials, theta_init):
-#        return
-    
-#     def evaluateEstimatorOn(estimator, images):
-#        return
-    
-#     def mapEstimatorAccuracy(estimator, sample_grid):
-#        return
-    
-#     def makeThetaGridSamples(theta, gridsize, nTrials):
-#        return
-    
 #     def superResolutionModel(theta, theta_err, res_factor):
 #        return
     
     ## These are protected methods but Python protects nothing!
-
-    def __check_image_theta(image,theta):
+    
+    # all input checks get routed through this function
+    def __check_inputs(input_dict):
+        for key, value in input_dict:
+            print(key)
         return
+
 #     def __estimate_GPUGaussMLE(image):
 #        return
     
 #     def __estimateDebug_GPUGaussMLE(image):
 #        return
     
-#     def __estimate_fminsearch(image, theta_init):
-#        return
-    
-#     def __estimateDebug_fminsearch(image, theta_init):
-#        return
-    
-#     def __estimate_toolbox(image, theta_init, algorithm):
-#        return
-    
-#     def __estimateDebug_toolbox(image, theta_init, algorithm):
-#        return
-    
     ## these are static methods from the Matlab MappelBase class
-#     def __cholesky(A):
-#        return
-    
-#     def __modifiedCholesky(A):
-#        return
-    
-#     def __choleskySolve(A,b):
-#        return
-    
-#     def __viewDipImage(image, fig):
-#        return
     
 #     def __checkImage(image):
 #        return
