ef99efed2eda9622fffe0857e3c4827b9233b890
prelich <physx.grad@gmail.com>
2018-02-09 19:50:36 -0500

    Gauss1DMLE histograms for presentation

---
+++ python/mappel/histogram_plots.py
@@ -0,0 +1,9 @@
+# histogram_plots.py
+# working script for generating 1-D plots for my presentation using the MAPPEL engine
+
+import numpy as np
+import mappel
+
+M = mappel.Gauss1DMLE(8,1.0)
+
+P_samp = [3.5,10000,20]
