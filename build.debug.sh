2c16404117345763a6d70ae45187659c380ab489
Mark J. Olah <markjolah@users.noreply.github.com>
2018-06-04 17:18:23 -0600

    working on build system

--- build.debug.sh
+++ build.debug.sh
@@ -2,10 +2,10 @@
 
 INSTALL_PATH=_install
 BUILD_PATH=_build/Debug
-
+NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
+COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DOPT_PYTHON=1 -DOPT_TESTING=1 -DOPT_DEBUG=1"
 rm -rf $INSTALL_PATH $BUILD_PATH
-#rm -rf $BUILD_PATH
 set -e
 
-cmake -H. -B$BUILD_PATH -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Debug -DOPT_PYTHON=1 $@
-VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j8
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS} $@
+VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 INSTALL_PATH=_install
 BUILD_PATH=_build
-
+NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
+COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
 rm -rf $BUILD_PATH
 set -e
 
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Debug
-cmake -H. -B$BUILD_PATH/Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Release
-VERBOSE=1 cmake --build $BUILD_PATH/Debug --target install -- -j8
-VERBOSE=1 cmake --build $BUILD_PATH/Release --target install -- -j8
+cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS} 
+cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=1 ${COMMON_ARGS} 
+cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
+cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
