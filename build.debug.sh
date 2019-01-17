51bb1546b3111e9a7f03488e0b6929c090e9bd76
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-17 08:36:20 -0700

    Fix AddExternalProject warnings

--- build.debug.sh
+++ build.debug.sh
@@ -2,10 +2,14 @@
 
 INSTALL_PATH=_install
 BUILD_PATH=_build/Debug
-NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DOPT_PYTHON=1 -DOPT_TESTING=1 -DOPT_DEBUG=1"
-rm -rf $BUILD_PATH
-set -e
+NUM_PROCS=$(grep -c ^processor /proc/cpuinfo)
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_TESTING=Off"
+ARGS="${ARGS} -DOPT_DEBUG=On"
+ARGS="${ARGS} -DOPT_MATLAB=Off"
+ARGS="${ARGS} -DOPT_PYTHON=Off"
 
-cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS} $@
+set -ex
+rm -rf $BUILD_PATH $INSTALL_PATH
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug ${ARGS} $@
 VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
